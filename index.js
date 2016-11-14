f950b654b948bcf91c6d0d835a8c5afd0916703c
Diego Majuf <diego.majluf@gmail.com>
2016-11-14 08:36:19 -0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 "use strict";
 var dtes = require('./src/dtes');
 var firma = require('./src/firma_electronica');
+var periodos = require('./src/periodo');
+var responses = require('./src/responses');
 exports.dte = dtes;
 exports.firma = firma;
+exports.periodos = periodos;
+exports.responses = responses;
 //# sourceMappingURL=index.js.map
