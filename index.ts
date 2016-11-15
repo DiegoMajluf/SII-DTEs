3fb9b5760e58a3467fc39b52ba0db541e3d09bed
Diego Majluf <diego.majluf@gmail.com>
2016-11-15 12:36:12 -0300

    1

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as dtes from './src/dtes'
 import * as firma from './src/firma_electronica'
 import * as periodos from './src/periodo'
 import * as responses from './src/responses'
+import * as DteService from './src/dte-service'
 
 
 
@@ -9,3 +10,4 @@ export import dte = dtes;
 export import firma = firma;
 export import periodos = periodos;
 export import responses = responses;
+export import DteService = DteService;
