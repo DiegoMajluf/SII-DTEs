43287cc3459627f7610ef9f10082f4671dbf7d0c
Diego Majuf <diego.majluf@gmail.com>
2016-11-19 12:46:34 -0300

    1

--- src/app-data.ts
+++ src/app-data.ts
@@ -1,7 +1,7 @@
- export default {
-    RazonSocial: "Sales Manager SpA",
-    Fantasia: "SMART CRM",
+export default {
+    RAZON_SOCIAL: "Sales Manager SpA",
+    FANTASIA: "SMART CRM",
     RUT: 'XXXXXXXX-X',
-
+    HTTP_ROOT: 'http://localhost:3001/'
 
 }
