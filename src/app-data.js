71279d3bf2e7cb23f4f241a4fc2057d086c384a3
Diego Majuf <diego.majluf@gmail.com>
2016-11-19 12:53:08 -0300

    1

--- src/app-data.js
+++ src/app-data.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    RazonSocial: "Sales Manager SpA",
-    Fantasia: "SMART CRM",
+    RAZON_SOCIAL: "Sales Manager SpA",
+    FANTASIA: "SMART CRM",
     RUT: 'XXXXXXXX-X',
+    HTTP_ROOT: 'http://localhost:3001/',
+    HTTP_SII_ROOT: 'https://www1.sii.cl/cgi-bin/Portal001/'
 };
 //# sourceMappingURL=app-data.js.map
--- src/app-data.ts
+++ src/app-data.ts
@@ -2,6 +2,7 @@ export default {
     RAZON_SOCIAL: "Sales Manager SpA",
     FANTASIA: "SMART CRM",
     RUT: 'XXXXXXXX-X',
-    HTTP_ROOT: 'http://localhost:3001/'
+    HTTP_ROOT: 'http://localhost:3001/',
+    HTTP_SII_ROOT: 'https://www1.sii.cl/cgi-bin/Portal001/'
 
 }
