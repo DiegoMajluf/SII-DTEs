240cad6ddaedbb796c859a78e70f83780b25e339
Diego Majluf <diego.majluf@gmail.com>
2017-05-11 17:32:49 -0300

    map key

--- src/app-data.ts
+++ src/app-data.ts
@@ -3,6 +3,6 @@ export default {
     FANTASIA: "SMART CRM",
     RUT: 'XXXXXXXX-X',
     HTTP_ROOT: 'http://localhost:3001/',
-    HTTP_SII_ROOT: 'https://www1.sii.cl/cgi-bin/Portal001/'
-
+    HTTP_SII_ROOT: 'https://www1.sii.cl/cgi-bin/Portal001/',
+    MAP_KEY: "AIzaSyDaNJCYVV5L4IygY8Eaz_dFbnlgSt7mdCg"
 }
